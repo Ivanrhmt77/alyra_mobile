46334f23b77c76a2e5fec1e9d18153010e068f6e
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2025-12-19 14:26:38 +0700

    feat(theme): add app color palette

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  // ─────────── PRIMARY ───────────
+
+  static const Color primary100 = Color(0xFFEBF7FE);
+  static const Color primary200 = Color(0xFFBAE5FA);
+  static const Color primary300 = Color(0xFF7FCEF7);
+  static const Color primary400 = Color(0xFF4EBCF3);
+  static const Color primary500 = Color(0xFF3AB4F2);
+  static const Color primary600 = Color(0xFF2CAFF1);
+  static const Color primary700 = Color(0xFF0F9EE6);
+  static const Color primary800 = Color(0xFF0C7BB4);
+  static const Color primary900 = Color(0xFF08567D);
+  static const Color primary1000 = Color(0xFF05334B);
+
+  static const Color primaryAlpha10 = Color(0x1A3AB4F2);
+  static const Color primaryAlpha15 = Color(0x263AB4F2);
+  static const Color primaryAlpha20 = Color(0x333AB4F2);
+  static const Color primaryAlpha25 = Color(0x403AB4F2);
+  static const Color primaryAlpha30 = Color(0x4D3AB4F2);
+  static const Color primaryAlpha35 = Color(0x593AB4F2);
+  static const Color primaryAlpha40 = Color(0x663AB4F2);
+  static const Color primaryAlpha45 = Color(0x733AB4F2);
+  static const Color primaryAlpha50 = Color(0x803AB4F2);
+  static const Color primaryAlpha75 = Color(0xBF3AB4F2);
+
+  // ─────────── SECONDARY ───────────
+
+  static const Color secondary100 = Color(0xFFF0EEFA);
+  static const Color secondary200 = Color(0xFFCBC6F0);
+  static const Color secondary300 = Color(0xFFA096E2);
+  static const Color secondary400 = Color(0xFF7B6ED8);
+  static const Color secondary500 = Color(0xFF6C5DD3);
+  static const Color secondary600 = Color(0xFF6151D0);
+  static const Color secondary700 = Color(0xFF4735C4);
+  static const Color secondary800 = Color(0xFF382A99);
+  static const Color secondary900 = Color(0xFF271D6A);
+  static const Color secondary1000 = Color(0xFF17113F);
+
+  static const Color secondaryAlpha10 = Color(0x1A6C5DD3);
+  static const Color secondaryAlpha15 = Color(0x266C5DD3);
+  static const Color secondaryAlpha20 = Color(0x336C5DD3);
+  static const Color secondaryAlpha25 = Color(0x406C5DD3);
+  static const Color secondaryAlpha30 = Color(0x4D6C5DD3);
+  static const Color secondaryAlpha35 = Color(0x596C5DD3);
+  static const Color secondaryAlpha40 = Color(0x666C5DD3);
+  static const Color secondaryAlpha45 = Color(0x736C5DD3);
+  static const Color secondaryAlpha50 = Color(0x806C5DD3);
+  static const Color secondaryAlpha75 = Color(0xBF6C5DD3);
+
+  // ─────────── NEUTRAL ───────────
+
+  static const Color neutral100 = Color(0xFFEEEEEE);
+  static const Color neutral200 = Color(0xFFE3E3E3);
+  static const Color neutral300 = Color(0xFFC6C6C6);
+  static const Color neutral400 = Color(0xFFAAAAAA);
+  static const Color neutral500 = Color(0xFF8E8E8E);
+  static const Color neutral600 = Color(0xFF717171);
+  static const Color neutral700 = Color(0xFF555555);
+  static const Color neutral800 = Color(0xFF393939);
+  static const Color neutral900 = Color(0xFF1C1C1C);
+  static const Color neutral1000 = Color(0xFF111111);
+
+  static const Color neutralAlpha10 = Color(0x1A111111);
+  static const Color neutralAlpha15 = Color(0x26111111);
+  static const Color neutralAlpha20 = Color(0x33111111);
+  static const Color neutralAlpha25 = Color(0x40111111);
+  static const Color neutralAlpha30 = Color(0x4D111111);
+  static const Color neutralAlpha35 = Color(0x59111111);
+  static const Color neutralAlpha40 = Color(0x66111111);
+  static const Color neutralAlpha45 = Color(0x73111111);
+  static const Color neutralAlpha50 = Color(0x80111111);
+  static const Color neutralAlpha75 = Color(0xBF111111);
+
+  // ─────────── ERROR ───────────
+
+  static const Color error100 = Color(0xFFFCEAEA);
+  static const Color error200 = Color(0xFFF6B9B8);
+  static const Color error300 = Color(0xFFEE7F7C);
+  static const Color error400 = Color(0xFFE84E4A);
+  static const Color error500 = Color(0xFFE53935);
+  static const Color error600 = Color(0xFFE32D28);
+  static const Color error700 = Color(0xFFCD1E1A);
+  static const Color error800 = Color(0xFFA01815);
+  static const Color error900 = Color(0xFF70110E);
+  static const Color error1000 = Color(0xFF440A09);
+
+  static const Color errorAlpha10 = Color(0x1AE53935);
+  static const Color errorAlpha15 = Color(0x26E53935);
+  static const Color errorAlpha20 = Color(0x33E53935);
+  static const Color errorAlpha25 = Color(0x40E53935);
+  static const Color errorAlpha30 = Color(0x4DE53935);
+  static const Color errorAlpha35 = Color(0x59E53935);
+  static const Color errorAlpha40 = Color(0x66E53935);
+  static const Color errorAlpha45 = Color(0x73E53935);
+  static const Color errorAlpha50 = Color(0x80E53935);
+  static const Color errorAlpha75 = Color(0xBFE53935);
+
+  // ─────────── WARNING ───────────
+
+  static const Color warning100 = Color(0xFFFFF9EC);
+  static const Color warning200 = Color(0xFFFFEBBD);
+  static const Color warning300 = Color(0xFFFFD980);
+  static const Color warning400 = Color(0xFFFFC742);
+  static const Color warning500 = Color(0xFFFFB913);
+  static const Color warning600 = Color(0xFFFFB300);
+  static const Color warning700 = Color(0xFFEEA700);
+  static const Color warning800 = Color(0xFFC38900);
+  static const Color warning900 = Color(0xFF8C6200);
+  static const Color warning1000 = Color(0xFF553C00);
+
+  static const Color warningAlpha10 = Color(0x1AFFB300);
+  static const Color warningAlpha15 = Color(0x26FFB300);
+  static const Color warningAlpha20 = Color(0x33FFB300);
+  static const Color warningAlpha25 = Color(0x40FFB300);
+  static const Color warningAlpha30 = Color(0x4DFFB300);
+  static const Color warningAlpha35 = Color(0x59FFB300);
+  static const Color warningAlpha40 = Color(0x66FFB300);
+  static const Color warningAlpha45 = Color(0x73FFB300);
+  static const Color warningAlpha50 = Color(0x80FFB300);
+  static const Color warningAlpha75 = Color(0xBFFFB300);
+
+  // ─────────── SUCCESS ───────────
+
+  static const Color success100 = Color(0xFFCEFFED);
+  static const Color success200 = Color(0xFF9BFFDA);
+  static const Color success300 = Color(0xFF61FFC4);
+  static const Color success400 = Color(0xFF27FFAF);
+  static const Color success500 = Color(0xFF00F399);
+  static const Color success600 = Color(0xFF00D083);
+  static const Color success700 = Color(0xFF00C27A);
+  static const Color success800 = Color(0xFF00A869);
+  static const Color success900 = Color(0xFF006E45);
+  static const Color success1000 = Color(0xFF003421);
+
+  static const Color successAlpha10 = Color(0x1A00C27A);
+  static const Color successAlpha15 = Color(0x2600C27A);
+  static const Color successAlpha20 = Color(0x3300C27A);
+  static const Color successAlpha25 = Color(0x4000C27A);
+  static const Color successAlpha30 = Color(0x4D00C27A);
+  static const Color successAlpha35 = Color(0x5900C27A);
+  static const Color successAlpha40 = Color(0x6600C27A);
+  static const Color successAlpha45 = Color(0x7300C27A);
+  static const Color successAlpha50 = Color(0x8000C27A);
+  static const Color successAlpha75 = Color(0xBF00C27A);
+}
