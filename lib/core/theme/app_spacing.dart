0dd2999f87723d68d415b80558b25cc0272e0252
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2025-12-19 21:45:32 +0700

    feat(theme): add horizontal & vertical spacing

---
+++ lib/core/theme/app_spacing.dart
@@ -0,0 +1,33 @@
+import 'package:alyra_mobile/core/theme/responsive.dart';
+
+class AppSpacing {
+  const AppSpacing._();
+
+  // ===== HORIZONTAL =====
+
+  static double get h2 => Responsive.w(2);
+  static double get h4 => Responsive.w(4);
+  static double get h8 => Responsive.w(8);
+  static double get h12 => Responsive.w(12);
+  static double get h16 => Responsive.w(16);
+  static double get h20 => Responsive.w(20);
+  static double get h24 => Responsive.w(24);
+  static double get h32 => Responsive.w(32);
+  static double get h40 => Responsive.w(40);
+  static double get h48 => Responsive.w(48);
+  static double get h56 => Responsive.w(56);
+
+  // ===== VERTICAL =====
+
+  static double get v2 => Responsive.h(2);
+  static double get v4 => Responsive.h(4);
+  static double get v8 => Responsive.h(8);
+  static double get v12 => Responsive.h(12);
+  static double get v16 => Responsive.h(16);
+  static double get v20 => Responsive.h(20);
+  static double get v24 => Responsive.h(24);
+  static double get v32 => Responsive.h(32);
+  static double get v40 => Responsive.h(40);
+  static double get v48 => Responsive.h(48);
+  static double get v56 => Responsive.h(56);
+}
